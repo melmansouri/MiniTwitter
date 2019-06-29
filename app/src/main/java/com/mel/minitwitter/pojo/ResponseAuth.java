662c9811956f4cbb27dde0df91f6201fb815fa0b
aymane <mohamedelmanssouri13@gmail.com>
2019-06-29 19:25:59 +0200

    Completed auths screens. Created pojos for login and signup

---
+++ app/src/main/java/com/mel/minitwitter/pojo/ResponseAuth.java
@@ -0,0 +1,67 @@
+package com.mel.minitwitter.pojo;
+
+public class ResponseAuth {
+    private String token;
+    private String username;
+    private String email;
+    private String role;//Only for signup
+    private String photoUrl;
+    private String created;
+    private boolean active;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
