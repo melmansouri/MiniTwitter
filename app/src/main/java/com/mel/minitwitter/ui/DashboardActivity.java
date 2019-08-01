0820ceb591f506bdc82f693b3287995711908cbc
aymane <mohamedelmanssouri13@gmail.com>
2019-08-02 00:28:14 +0200

    Implementada la vista para añadir nuevos tweets

--- app/src/main/java/com/mel/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/mel/minitwitter/ui/DashboardActivity.java
@@ -1,18 +1,23 @@
 package com.mel.minitwitter.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.mel.minitwitter.R;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-
-import android.view.MenuItem;
-import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -35,11 +40,18 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ButterKnife.bind(this);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         getSupportActionBar().hide();
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer,new TweetListFragment())
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, new TweetListFragment())
                 .commit();
+    }
+
+    @OnClick(R.id.fab)
+    public void newTweetDialog(){
+        NuevoTweetDialogFragment fragment=new NuevoTweetDialogFragment();
+        fragment.show(getSupportFragmentManager(),"NuevoTweetDialogFragment");
     }
 
 }
---
+++ app/src/main/java/com/mel/minitwitter/ui/NuevoTweetDialogFragment.java
@@ -0,0 +1,34 @@
+package com.mel.minitwitter.ui;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mel.minitwitter.R;
+
+public class NuevoTweetDialogFragment extends DialogFragment {
+
+
+    public NuevoTweetDialogFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL,R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_nuevo_tweet_dialog, container, false);
+    }
+
+}
